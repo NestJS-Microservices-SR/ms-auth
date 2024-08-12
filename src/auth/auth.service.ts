36c3b05db2bbfc3b18062b222bef5358a1b80380
soRodriguezz <s.rodriguezzapata01@gmail.com>
2024-08-12 11:36:25 -0400

    Add user registration functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { RegisterUserDTO } from './dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -12,5 +14,41 @@ export class AuthService extends PrismaClient implements OnModuleInit {
   onModuleInit() {
     this.$connect();
     this.logger.log('Prisma connected');
+  }
+
+  async registerUser(registerUserDto: RegisterUserDTO) {
+    const { email, name, password } = registerUserDto;
+    try {
+      const user = await this.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User already exists',
+        });
+      }
+
+      const newUser = this.user.create({
+        data: {
+          email,
+          name,
+          password,
+        },
+      });
+
+      return {
+        user: newUser,
+        token: 'abc'
+      }
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message,
+      });
+    }
   }
 }
